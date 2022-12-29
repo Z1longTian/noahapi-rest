60224a8106e7d586c5603942e0c61dc6572d39d2
Zilong Tian <tobywoodman@protonmail.com>
2022-12-29 20:21:55 +0000

    minor bug

--- src/models/nft.js
+++ src/models/nft.js
@@ -24,7 +24,7 @@ const nft = mongoose.Schema(
             required: true
         },
         tokenid: {
-            type: Number,
+            type: String,
             default: 0
         },
         views: {
